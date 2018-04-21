e678d105686486e8cb6cc8c5b9d40daf0d5c5dc4
Jaden Weiss <jadr2ddude@gmail.com>
2018-04-21 19:10:07 -0400

    add http request to handler

--- handler.go
+++ handler.go
@@ -4,7 +4,7 @@ import "net/http"
 
 //Handler is an http.Handler which uses SSE.
 //If there is an error in NewSender, an http.StatusInternalServerError is sent.
-type Handler func(*Sender)
+type Handler func(*Sender, *http.Request)
 
 func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	s, err := NewSender(w)
@@ -12,5 +12,5 @@ func (h Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		return
 	}
-	h(s)
+	h(s, r)
 }
