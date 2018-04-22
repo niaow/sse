710ebd178137963a4abcf6e6e68664960cb3576f
oliverpool <oliverpool@hotmail.fr>
2018-04-22 20:13:59 +0200

    document client parsing

--- client.go
+++ client.go
@@ -33,27 +33,33 @@ func (c *Client) Event() (ev Event, err error) {
 		return Event{}, ErrClosedClient
 	}
 
+	// According to https://html.spec.whatwg.org/multipage/server-sent-events.html#dispatchMessage
 	var event ScannedEvent
-	// Wait for an event with some data
+
+	// "2. If the data buffer is an empty string (...) return" (ie: don't dispatch an event)
 	for event.Data == "" {
 		event, err = c.s.Event()
+		// todo: set the event stream's reconnection time
 		if err != nil {
 			return Event{}, err
 		}
+		// "1. Set the last event ID string of the event source"
 		if event.IDSet {
 			c.lastEventID = event.ID
 		}
 	}
 	if event.Type == "" {
+		// 5. Initialize event's type attribute to message
 		ev.Name = "message"
 	} else {
+		// 6. If the event type buffer has a value other than the empty string
 		ev.Name = event.Type
 	}
 
-	// strip last \n
+	// 3. If the data buffer's last character is a \n (...) remove [it]
 	ev.Data = event.Data[:len(event.Data)-1]
 
-	// todo: set lastEventID of event
+	// todo: set lastEventID on event (5.)
 
 	return ev, nil
 }
