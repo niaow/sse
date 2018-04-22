2918bd15fd224a2748f4da0eae5e4b14a3f55a64
oliverpool <oliverpool@hotmail.fr>
2018-04-22 18:42:59 +0200

    make client testable

--- client.go
+++ client.go
@@ -10,15 +10,20 @@ import (
 
 //Client is a SSE client
 type Client struct {
-	r io.ReadCloser
+	r io.Reader
 	s *bufio.Scanner
+}
+
+// NewClient returns a client which will parse Event from the io.Reader
+func NewClient(r io.Reader) *Client {
+	return &Client{r: r, s: bufio.NewScanner(r)}
 }
 
 //ErrClosedClient is an error indicating that the client has been used after it was closed.
 var ErrClosedClient = errors.New("client closed")
 
-//ReadEvent reads an event from the stream.
-func (c *Client) ReadEvent() (ev Event, err error) {
+// Event reads an event from the stream.
+func (c *Client) Event() (ev Event, err error) {
 	//An exact implementation of https://www.w3.org/TR/2009/WD-eventsource-20090421/#event-stream-interpretations
 	if c.s == nil {
 		return Event{}, ErrClosedClient
@@ -67,7 +72,11 @@ func (c *Client) ReadEvent() (ev Event, err error) {
 //Close closes the client
 func (c *Client) Close() error {
 	c.s = nil
-	return c.r.Close()
+	closer, ok := (c.r).(io.Closer)
+	if !ok {
+		return nil
+	}
+	return closer.Close()
 }
 
 //ErrNotSSE is an error returned when a client recieves a non-SSE response
---
+++ client_test.go
@@ -0,0 +1,86 @@
+package sse_test
+
+import (
+	"bytes"
+	"io"
+	"testing"
+
+	"github.com/jadr2ddude/sse"
+)
+
+func TestEventDecoding(t *testing.T) {
+	cc := []struct {
+		name  string
+		input string
+		event sse.Event
+		err   error
+	}{
+		{
+			name:  "empty_event",
+			input: "",
+			err:   io.EOF,
+		},
+		{
+			name:  "incomplete_event",
+			input: "data:ok\n",
+			err:   io.EOF,
+		},
+		{
+			name:  "one data line",
+			input: "data:ok\n\n",
+			event: sse.Event{Data: "ok"},
+		},
+		{
+			name:  "one data line with leading space",
+			input: "data: ok\n\n",
+			event: sse.Event{Data: "ok\n"},
+		},
+		{
+			name:  "one data line with two leading spaces",
+			input: "data:  ok\n\n",
+			event: sse.Event{Data: " ok\n"},
+		},
+		{
+			name:  "comment at the beginning",
+			input: ":some comment\ndata:ok\n\n",
+			event: sse.Event{Data: "ok\n"},
+		},
+		{
+			name:  "comment at the end",
+			input: "data:ok\n:some comment\n\n",
+			event: sse.Event{Data: "ok\n"},
+		},
+		{
+			name:  "empty data",
+			input: "data:\n\n",
+			event: sse.Event{Data: "\n"},
+		},
+		{
+			name:  "empty data (without ':')",
+			input: "data\n\n",
+			event: sse.Event{Data: "\n"},
+		},
+		{
+			name:  "multiple data lines",
+			input: "data:1\ndata: 2\ndata:3\n\n",
+			event: sse.Event{Data: "1\n2\n3\n"},
+		},
+	}
+
+	for _, c := range cc {
+		t.Run(c.name, func(t *testing.T) {
+			client := sse.NewClient(bytes.NewBufferString(c.input))
+			e, err := client.Event()
+			if err != c.err {
+				t.Errorf("got error '%v', expected '%v'", err, c.err)
+			}
+			if e != c.event {
+				t.Errorf("got %#v, expected %#v", e, c.event)
+			}
+			err = client.Close()
+			if err != nil {
+				t.Errorf("could not close the client: %v", err)
+			}
+		})
+	}
+}
