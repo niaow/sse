2ef93a6e34fd816482dd13600a16c60c4f5ee8b1
Jaden Weiss <jadr2ddude@gmail.com>
2018-04-22 13:51:57 -0400

    fix tests

--- client_test.go
+++ client_test.go
@@ -1,18 +1,16 @@
-package sse_test
+package sse
 
 import (
 	"bytes"
 	"io"
 	"testing"
-
-	"github.com/jadr2ddude/sse"
 )
 
 func TestEventDecoding(t *testing.T) {
 	cc := []struct {
 		name  string
 		input string
-		event sse.Event
+		event Event
 		err   error
 	}{
 		{
@@ -28,48 +26,48 @@ func TestEventDecoding(t *testing.T) {
 		{
 			name:  "one data line",
 			input: "data:ok\n\n",
-			event: sse.Event{Data: "ok"},
+			event: Event{Data: "ok"},
 		},
 		{
 			name:  "one data line with leading space",
 			input: "data: ok\n\n",
-			event: sse.Event{Data: "ok"},
+			event: Event{Data: "ok"},
 		},
 		{
 			name:  "one data line with two leading spaces",
 			input: "data:  ok\n\n",
-			event: sse.Event{Data: " ok"},
+			event: Event{Data: " ok"},
 		},
 		{
 			name:  "comment at the beginning",
 			input: ":some comment\ndata:ok\n\n",
-			event: sse.Event{Data: "ok"},
+			event: Event{Data: "ok"},
 		},
 		{
 			name:  "comment at the end",
 			input: "data:ok\n:some comment\n\n",
-			event: sse.Event{Data: "ok"},
+			event: Event{Data: "ok"},
 		},
 		{
 			name:  "empty data",
 			input: "data:\n\n",
-			event: sse.Event{Data: ""},
+			event: Event{Data: ""},
 		},
 		{
 			name:  "empty data (without ':')",
 			input: "data\n\n",
-			event: sse.Event{Data: ""},
+			event: Event{Data: ""},
 		},
 		{
 			name:  "multiple data lines",
 			input: "data:1\ndata: 2\ndata:3\n\n",
-			event: sse.Event{Data: "1\n2\n3"},
+			event: Event{Data: "1\n2\n3"},
 		},
 	}
 
 	for _, c := range cc {
 		t.Run(c.name, func(t *testing.T) {
-			client := sse.NewClient(bytes.NewBufferString(c.input))
+			client := NewClient(bytes.NewBufferString(c.input))
 			e, err := client.Event()
 			if err != c.err {
 				t.Errorf("got error '%v', expected '%v'", err, c.err)
--- scanner_test.go
+++ scanner_test.go
@@ -1,18 +1,16 @@
-package sse_test
+package sse
 
 import (
 	"bytes"
 	"io"
 	"testing"
-
-	"github.com/jadr2ddude/sse"
 )
 
 func TestScannedEventDecoding(t *testing.T) {
 	cc := []struct {
 		name  string
 		input string
-		event sse.ScannedEvent
+		event ScannedEvent
 		err   error
 	}{
 		{
@@ -28,68 +26,68 @@ func TestScannedEventDecoding(t *testing.T) {
 		{
 			name:  "one data line",
 			input: "data:ok\n\n",
-			event: sse.ScannedEvent{Data: "ok\n"},
+			event: ScannedEvent{Data: "ok\n"},
 		},
 		{
 			name:  "one data line with retry",
 			input: "data:ok\nretry:15\n\n",
-			event: sse.ScannedEvent{Data: "ok\n", Retry: 15},
+			event: ScannedEvent{Data: "ok\n", Retry: 15},
 		},
 		{
 			name:  "one data line with type",
 			input: "event: type\ndata:ok\n\n",
-			event: sse.ScannedEvent{Type: "type", Data: "ok\n"},
+			event: ScannedEvent{Type: "type", Data: "ok\n"},
 		},
 		{
 			name:  "one data line with id",
 			input: "data:ok\nid:1\n\n",
-			event: sse.ScannedEvent{ID: "1", Data: "ok\n"},
+			event: ScannedEvent{ID: "1", Data: "ok\n"},
 		},
 		{
 			name:  "U+0000 in ID",
 			input: "id:\0001\n\n",
-			event: sse.ScannedEvent{},
+			event: ScannedEvent{},
 		},
 		{
 			name:  "one data line with leading space",
 			input: "data: ok\n\n",
-			event: sse.ScannedEvent{Data: "ok\n"},
+			event: ScannedEvent{Data: "ok\n"},
 		},
 		{
 			name:  "one data line with two leading spaces",
 			input: "data:  ok\n\n",
-			event: sse.ScannedEvent{Data: " ok\n"},
+			event: ScannedEvent{Data: " ok\n"},
 		},
 		{
 			name:  "comment at the beginning",
 			input: ":some comment\ndata:ok\n\n",
-			event: sse.ScannedEvent{Data: "ok\n"},
+			event: ScannedEvent{Data: "ok\n"},
 		},
 		{
 			name:  "comment at the end",
 			input: "data:ok\n:some comment\n\n",
-			event: sse.ScannedEvent{Data: "ok\n"},
+			event: ScannedEvent{Data: "ok\n"},
 		},
 		{
 			name:  "empty data",
 			input: "data:\n\n",
-			event: sse.ScannedEvent{Data: "\n"},
+			event: ScannedEvent{Data: "\n"},
 		},
 		{
 			name:  "empty data (without ':')",
 			input: "data\n\n",
-			event: sse.ScannedEvent{Data: "\n"},
+			event: ScannedEvent{Data: "\n"},
 		},
 		{
 			name:  "multiple data lines",
 			input: "data:1\ndata: 2\ndata:3\n\n",
-			event: sse.ScannedEvent{Data: "1\n2\n3\n"},
+			event: ScannedEvent{Data: "1\n2\n3\n"},
 		},
 	}
 
 	for _, c := range cc {
 		t.Run(c.name, func(t *testing.T) {
-			scanner := sse.NewScanner(bytes.NewBufferString(c.input))
+			scanner := NewScanner(bytes.NewBufferString(c.input))
 			e, err := scanner.Event()
 			if err != c.err {
 				t.Errorf("got error '%v', expected '%v'", err, c.err)
